0074ad937205513210378698973f1fed96f70d8f
2XXE (SRA) <40869774+2XXE-SRA@users.noreply.github.com>
2022-12-07 15:35:21 -0500

    Create bazar_adf.bat

---
+++ batch/bazar_adf.bat
@@ -0,0 +1,9 @@
+:: ADFind commands per https://unit42.paloaltonetworks.com/bazarloader-network-reconnaissance/
+
+adfind.exe -f "(objectcategory=person)" > ad_users.txt
+adfind.exe -f "objectcategory=computer" > ad_computers.txt
+adfind.exe -f "(objectcategory=organizationalUnit)" > ad_ous.txt
+adfind.exe -sc trustdmp > trustdmp.txt
+adfind.exe -subnets -f (objectCategory=subnet)> subnets.txt
+adfind.exe -f "(objectcategory=group)" > ad_group.txt
+adfind.exe -gcb -sc trustdmp > trustdmp.txt
