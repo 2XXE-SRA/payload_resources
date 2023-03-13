c3302438a89ca718ad9d488887c66465fa653794
2XXE (SRA) <40869774+2XXE-SRA@users.noreply.github.com>
2023-03-13 10:25:27 -0400

    Update hive_defender.bat

--- batch/hive_defender.bat
+++ batch/hive_defender.bat
@@ -5,7 +5,7 @@ reg.exe add "HKLM\System\CurrentControlSet\Services\SecurityHealthService" /v "S
 reg.exe delete "HKLM\Software\Policies\Microsoft\Windows Defender" /f 
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f 
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d "1" /f 
-reg.exe add HKLM\Software\Policies\Microsoft\Windows Defender\MpEngine" /v "MpEnablePus" /t REG_DWORD /d "0" /f 
+reg.exe add "HKLM\Software\Policies\Microsoft\Windows Defender\MpEngine" /v "MpEnablePus" /t REG_DWORD /d "0" /f 
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "1" /f 
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableIOAVProtection" /t REG_DWORD /d "1" /f 
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "1" /f 
